421ef2a8c21f4d8b5d3c8acf4c513e2f083f8980
fgprodigal <fgprodigal@gmail.com>
2020-02-09 18:07:53 +0800

    Update

--- diy.sh
+++ diy.sh
@@ -6,6 +6,27 @@
 # Blog: https://p3terx.com
 #=================================================
 
+echo 'src-git lienol https://github.com/Lienol/openwrt-package' >> feeds.conf.default
+./scripts/feeds clean
+./scripts/feeds update -a
+rm -rf feeds/lienol/lienol/ipt2socks
+rm -rf feeds/lienol/lienol/shadowsocksr-libev
+rm -rf feeds/lienol/lienol/pdnsd-alt
+rm -rf feeds/lienol/package/verysync
+rm -rf feeds/lienol/lienol/luci-app-verysync
+rm -rf package/lean/kcptun
+rm -rf package/lean/trojan
+rm -rf package/lean/v2ray
+rm -rf package/lean/luci-app-kodexplorer
+rm -rf package/lean/luci-app-pppoe-relay
+rm -rf package/lean/luci-app-pptp-server
+rm -rf package/lean/luci-app-v2ray-server
+./scripts/feeds install -a
+
+ln -sf ../../../feeds/lienol/package/trojan package/feeds/lienol/trojan
+ln -sf ../../../feeds/lienol/package/kcptun package/feeds/lienol/kcptun
+ln -sf ../../../feeds/lienol/package/v2ray package/feeds/lienol/v2ray
+
 rm -rf package/lean/luci-theme-argon
 git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
 
@@ -17,7 +38,12 @@ sed -i '/$ipt -A SS_SPEC_TPROXY -p udp -d $SERVER -j RETURN/a \
         $ipt -A SS_SPEC_TPROXY -p udp -m ipp2p --edk --bit --kazaa --gnu -j RETURN
 ' package/lean/luci-app-ssr-plus/root/usr/bin/ssr-rules
 
-#./scripts/feeds update -a
-#./scripts/feeds install -a
+sed -i '/$iptables_nat -N PSW_ACL/i \
+	$iptables_nat -A PSW -m ipp2p --edk --dc --kazaa --gnu --bit --apple --winmx --soul --ares -j RETURN
+' feeds/lienol/lienol/luci-app-passwall/root/usr/share/passwall/iptables.sh
+
+sed -i '/$iptables_mangle -N PSW_ACL/i \
+	$iptables_mangle -A PSW -p udp -m ipp2p --edk --bit --kazaa --gnu -j RETURN
+' feeds/lienol/lienol/luci-app-passwall/root/usr/share/passwall/iptables.sh
 
 sed -i 's/ +luci-theme-bootstrap//g' feeds/luci/collections/luci/Makefile
